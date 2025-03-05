7628f228c9904dab34d5ae063f34002b87731cb0
Jannik Zinkl <jannik@trieb.work>
2025-03-05 22:13:29 +0100

    fix: importmap

--- src/app/(payload)/admin/importMap.js
+++ src/app/(payload)/admin/importMap.js
@@ -38,6 +38,7 @@ import { default as default_0a2ba52d3c78797a6b0937e7611782e5 } from '@/component
 import { default as default_8e5a5a130d676dd8877220438eebccb4 } from '@/components/AdminDashboard/LoginButton'
 import { default as default_2f704de5e6c59b8917a26e353bdf0747 } from '@/components/AdminDashboard/BeforeDashboard'
 import { default as default_5abc63c3cf3fe7a656266cc0318fce13 } from '@/components/AdminDashboard/BeforeLogin'
+import { VercelBlobClientUploadHandler as VercelBlobClientUploadHandler_16c82c5e25f430251a3e3ba57219ff4e } from '@payloadcms/storage-vercel-blob/client'
 
 export const importMap = {
   "@/components/AdminDashboard/IconSelect#default": default_9d2d71c3c9804a7eac9f5a33fe39c4ed,
@@ -79,5 +80,6 @@ export const importMap = {
   "@/components/AdminDashboard/BackupDashboard#default": default_0a2ba52d3c78797a6b0937e7611782e5,
   "@/components/AdminDashboard/LoginButton#default": default_8e5a5a130d676dd8877220438eebccb4,
   "@/components/AdminDashboard/BeforeDashboard#default": default_2f704de5e6c59b8917a26e353bdf0747,
-  "@/components/AdminDashboard/BeforeLogin#default": default_5abc63c3cf3fe7a656266cc0318fce13
+  "@/components/AdminDashboard/BeforeLogin#default": default_5abc63c3cf3fe7a656266cc0318fce13,
+  "@payloadcms/storage-vercel-blob/client#VercelBlobClientUploadHandler": VercelBlobClientUploadHandler_16c82c5e25f430251a3e3ba57219ff4e
 }
